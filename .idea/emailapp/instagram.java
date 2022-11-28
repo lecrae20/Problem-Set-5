80c66ff4e8ab3df7f8401dc865e41bba51873214
etvirk <112572017+etvirk@users.noreply.github.com>
2022-11-28 12:00:12 -0500

    Update instagram.java

--- .idea/emailapp/instagram.java
+++ .idea/emailapp/instagram.java
@@ -3,5 +3,8 @@ package emailapp;
 import java.util.Scanner;
 
 public class instagram extends Socialmedia{
+  System.out.println("**********************");
+  System.out.println("Your Username: " +username);
+  Stystem.out.println("Your password: " + password);
   
 }
