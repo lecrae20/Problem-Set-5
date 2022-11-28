73237d1db3ada0f9b95662972d23c097cb67338d
etvirk <112572017+etvirk@users.noreply.github.com>
2022-11-28 11:54:33 -0500

    Update Socialmedia.java

--- .idea/emailapp/Socialmedia.java
+++ .idea/emailapp/Socialmedia.java
@@ -8,41 +8,49 @@ public class Socialmedia {
     public String password;
     public int defaultpasswordLength=8;
     public String country;
+
     public String username;
     public int mailboxCapactity=500;
     public String alternateEmail;
     public String companySuffix="aeycompany.com";
     public int age;
-    
-    public age(){
-        System.out.println("Enter age: ");
-        int age;
-        Scanner scnr=new Scanner(System.in);
-        age=scnr.nextInt();
-        return age;
-    }
 
-    public void username(String firstname, String lastname){
-        this.age=age;
-        this.firstname=firstname.charAt(0);
-        this.lastname=lastname.charAt(0)+lastname.charAt(1)+lastname.charAt(2);
-        System.out.println("Username Created: " + this.firstname+" " + this.lastname+this.age);
-        this.country=setCountry();
+    public Socialmedia(String firstname, String lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.setCountry();
         System.out.println("Country" + this.country);
         this.password=randomPassword(defaultpasswordLength);
         System.out.println("Your password is: " + this.password);
-        //email=firstname.toLowerCase()+"."+lastname.toLowerCase()+"@"+ country+"."+companySuffix;
+        this.setUsername(this.firstname, this.lastname);
+        this.setAge();
 
     }
 
-    public String Country(){
+    public void setAge(){
+        System.out.println("Enter age: ");
+        Scanner scnr=new Scanner(System.in);
+        this.age =  scnr.nextInt();
+
+    }
+
+
+
+    public void setUsername(String firstname, String lastname) {
+        this.firstname = "" + firstname.charAt(0);
+        this.lastname = lastname.substring(0, 3);
+        System.out.println("Username Created: " + this.firstname + " " + this.lastname + this.age);
+    }
+
+
+    public void setCountry(){
         System.out.println("New worker: " + firstname+ ". Country codes:\n1 for USA\n2 for England\n3 for Canada \n0 for Mexico\nEnter country code: ");
         Scanner in=new Scanner(System.in);
         int depChoice=in.nextInt();
-        if(depChoice==1){return "USA"; }
-        else if(depChoice==2){return "England";}
-        else if(depChoice==3){return "Canada";}
-        else{return "Mexico";}
+        if(depChoice==1){country = "USA"; }
+        else if(depChoice==2){country = "England";}
+        else if(depChoice==3){country = "Canada";}
+        else{country = "Mexico";}
     }
     public String randomPassword(int length){
         String passwordSet="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%";
@@ -82,7 +90,7 @@ public class Socialmedia {
 
     public String showInfo(){
         return "DISPLAY NAME: " + firstname+" "+lastname+
-                "\n SOCIAL MEDIA USERNAME: "+username+
+                "\nCOMPANY EMAIL: "+username+
                 "\nMAILBOX CAPACITY: " + mailboxCapactity+ "mb";
     }
 
