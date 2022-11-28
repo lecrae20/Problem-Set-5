7f57dd7f6d9ec8668402ab1e9968cb367a74b41f
etvirk <112572017+etvirk@users.noreply.github.com>
2022-11-28 11:01:21 -0500

    Update and rename EmailApp.java to Socialmediaapp.java

---
+++ .idea/emailapp/Socialmediaapp.java
@@ -0,0 +1,11 @@
+package emailapp;
+
+public class Socialmediaapp {
+    public static void main(String[] args) {
+        Socialmedia sm1=new Socialmedia("John","Smith");
+        System.out.println(sm1.showInfo());
+
+
+
+    }
+}
