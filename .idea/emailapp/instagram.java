dbf7b793c8c10402118d317b1f6e29e23c99ece2
emmadunnee <112571974+emmadunnee@users.noreply.github.com>
2022-11-30 11:18:26 -0500

    Create instagram.java

--- .idea/emailapp/instagram.java
+++ .idea/emailapp/instagram.java
@@ -1,19 +1,17 @@
 package emailapp;
-import java.awt.*;
 
 import java.util.Scanner;
 
 public class instagram extends Socialmedia{
 Scanner scnr=new Scanner(System.in);
-  StdDraw.setScale(-0.1, 7.1);
   String song="";
   
   System.out.println("**********************");
   
-  System.out.println("Your Username: " +username);
+  System.out.println("Your Username: " + username);
   System.out.println("Your password: " + password);
   
   System.out.println("******* MY BIO ******");
-  System.out.print("Hello! My name is " + firstname + " " + lastname + "! " + "I am a " + age + " year old from " + country + "! " + "Welcome to my page!");
-  System.out.print(" My favorite song is" + song);
+  System.out.print("Hello! My name is " + firstname + " " + lastname + "! " + "I am a " + age + " year old from " + country + "! Welcome to my page!");
+  System.out.print("My favorite song is " + song);
 }
