2c531b71fd7e83cfba88e2607e719a5c1d4c10dc
emmadunnee <112571974+emmadunnee@users.noreply.github.com>
2022-11-30 11:05:38 -0500

    Update instagram.java

--- .idea/emailapp/instagram.java
+++ .idea/emailapp/instagram.java
@@ -8,6 +8,6 @@ Scanner scnr=new Scanner(System.in);
   System.out.println("**********************");
   
   System.out.println("Your Username: " +username);
-  Stystem.out.println("Your password: " + password);
+  System.out.println("Your password: " + password);
   
 }
