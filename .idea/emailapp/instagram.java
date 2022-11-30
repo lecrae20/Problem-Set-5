f711d3c5b799ebd037e9fe24dbcf7ff1c5821f46
etvirk <112572017+etvirk@users.noreply.github.com>
2022-11-30 11:37:17 -0500

    Update instagram.java

--- .idea/emailapp/instagram.java
+++ .idea/emailapp/instagram.java
@@ -12,8 +12,10 @@ Scanner scnr=new Scanner(System.in);
   
   System.out.println("Your Username: " +username);
   System.out.println("Your password: " + password);
-  
+  System.out.println("Enter favorite song: ");
+  song=scnr.next();
   StdDraw.rectangle(3.5, 6.18, .45, .15);
+  
   
   StdDraw.text(3.5, 6.5, "******* MY BIO ******/n Hello! My name is " + firstname + " " + lastname + "! " + "I am a " + age + " year old from " + country + "! Welcome to my page!/n My favorite song is " + song);
   
