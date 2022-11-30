a72a7507d72cfe3d3cf8b83c7bf42e77b85da80c
lecrae20 <112572048+lecrae20@users.noreply.github.com>
2022-11-30 11:13:20 -0500

    Update instagram.java

--- .idea/emailapp/instagram.java
+++ .idea/emailapp/instagram.java
@@ -10,4 +10,6 @@ Scanner scnr=new Scanner(System.in);
   System.out.println("Your Username: " +username);
   System.out.println("Your password: " + password);
   
+  System.out.println("******* MY BIO ******");
+  System.out.print("Hello! My name is " + firstname + lastname + "! " + "I am a " + age + " year old from " + country + "! " + "Welcome to my page!");
 }
