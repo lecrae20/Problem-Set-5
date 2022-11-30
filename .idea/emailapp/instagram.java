7843a53a7e20b12309e6320baea0ea1f48280b2d
carolinebmcmahon <112572014+carolinebmcmahon@users.noreply.github.com>
2022-11-30 12:12:12 -0500

    Update instagram.java

--- .idea/emailapp/instagram.java
+++ .idea/emailapp/instagram.java
@@ -1,25 +1,18 @@
-package emailapp;
-import java.awt.*;
+package Socailmedia;
 
 import java.util.Scanner;
-
 public class instagram extends Socialmedia{
-Scanner scnr=new Scanner(System.in);
-  StdDraw.setScale(-0.1, 7.1);
-  String song="";
-  
-  System.out.println("**********************");
-  
-  System.out.println("Your Username: " +username);
-  System.out.println("Your password: " + password);
-  System.out.println("Enter favorite song: ");
-  song=scnr.next();
-  StdDraw.rectangle(3.5, 6.18, .45, .15);
-  
-  
-  StdDraw.text(3.5, 6.5, "******* MY BIO ******/n Hello! My name is " + firstname + " " + lastname + "! " + "I am a " + age + " year old from " + country + "! Welcome to my page!/n My favorite song is " + song);
-  
- // System.out.println("******* MY BIO ******");
-//  System.out.print("Hello! My name is " + firstname + " " + lastname + "! " + "I am a " + age + " year old from " + country + "! " + "Welcome to my page!");
- // System.out.print(" My favorite song is" + song);
+  public instagram(String firstname, String lastname){
+    Scanner scnr = new Scanner(System.in);
+    String song = "";
+
+    System.out.println("**********************");
+
+    System.out.println("Your Username: " + username);
+    System.out.println("Your password: " + password);
+
+    System.out.println("******* MY BIO ******");
+    System.out.print("Hello! My name is " + firstname + " " + lastname + "! " + "I am a " + age + " year old from " + country + "! Welcome to my page!");
+    System.out.print("My favorite song is " + song);
+  }
 }
